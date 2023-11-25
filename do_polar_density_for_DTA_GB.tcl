8c9605d103bd617032b99cb247b9fec53918fbf6
Grace Brannigan <gracebrannigan@gmail.com>
2023-11-24 22:36:01 -0500

    added wrapping script for running TCL calculations

---
+++ do_polar_density_for_DTA_GB.tcl
@@ -0,0 +1,20 @@
+
+set UTILS "../densitymap/TCL/helpers" 
+source ./polarDensity_for_DTA.tcl
+
+set HeadNames [atomselect top "name PO4"] ;#B2 is DDM
+set lipids [lsort -unique [$HeadNames get resname]]
+puts $lipids
+$HeadNames delete
+puts $lipids
+set Rmax 60.
+set Rmin 0.
+set dr 5.
+set Ntheta 30
+set dt 1
+set sample_frame 0
+
+source ${UTILS}/assign_helices_ELIC_CG.tcl
+foreach lip $lipids {
+	polarDensityBin $lip.dat $lip $Rmin $Rmax $dr $Ntheta $dt $sample_frame	[list A B C D E] [list 1 2 3 4]
+}
