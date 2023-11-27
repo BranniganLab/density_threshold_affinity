5858b8e88c47559fd34982fe69a3c9056965ea8a
EzryStIago <ezrysm@gmail.com>
2023-11-26 20:16:19 -0500

    now iterates over the entire field to get a better estimate of bulk counts

--- accessible_area/do_get_counts.tcl
+++ accessible_area/do_get_counts.tcl
@@ -1,10 +1,26 @@
 source get_counts.tcl
-set xmin 10
-set ymin 10
+set xmin 0
 
 set area 154
-set data [get_count_with_area $area $xmin $ymin "resname DPPC"]
 
-set outfile [open "counts_${area}.out" w+]    
-puts $outfile $data
+set step [expr $area**(0.5)]
+set box_width [lindex [pbc get] 0 0]
+set usable_width [expr $box_width - $step]
+
+set outfile [open "counts_${area}.out" w+]   
+
+
+while {$xmin < $usable_width} {
+	set ymin 0
+	while {$ymin < $usable_width} {
+		puts "Running at $xmin $ymin"
+		set data [get_count_with_area $area $xmin $ymin "resname DPPC"]
+		puts $outfile $data
+
+		set ymin [expr $ymin + $step]
+	}
+
+	set xmin [expr $xmin + $step]
+}
+
 close $outfile
