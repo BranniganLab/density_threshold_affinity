e2c87f7ed93e2c514cf57cecbcfdf0f20bba3b97
EzryStIago <ezrysm@gmail.com>
2023-11-27 14:08:15 -0500

    adding leaflet handling

--- accessible_area/do_get_counts.tcl
+++ accessible_area/do_get_counts.tcl
@@ -1,26 +1,46 @@
 source get_counts.tcl
-set xmin 0
 
+set species "DPPC"
+set midresid 1522
 set area 154
 
 set step [expr $area**(0.5)]
-set box_width [lindex [pbc get] 0 0]
-set usable_width [expr $box_width - $step]
 
-set outfile [open "counts_${area}.out" w+]   
+set all [atomselect top "name PO4"]
+set XMIN [lindex [measure minmax $all] 0 0]
+set YMIN [lindex [measure minmax $all] 0 1]
+set XMAX [lindex [measure minmax $all] 1 0]
+set YMAX [lindex [measure minmax $all] 1 1]
+
+set XMIN [expr $XMIN+$step]
+set YMIN [expr $YMIN+$step]
+set XMAX [expr $XMAX-$step]
+set YMAX [expr $YMAX-$step]
+
+set upper [atomselect top "resname $species and resid < $midresid"]
+$upper set beta 1
+set lower [atomselect top "resname $species and resid >= $midresid"]
+$lower set beta -1
 
 
-while {$xmin < $usable_width} {
-	set ymin 0
-	while {$ymin < $usable_width} {
-		puts "Running at $xmin $ymin"
-		set data [get_count_with_area $area $xmin $ymin "resname DPPC"]
-		puts $outfile $data
+set box_width [lindex [measure minmax $all] 1 0]
+$all delete
 
-		set ymin [expr $ymin + $step]
+set outfile [open "counts_${area}.out" w]   
+
+foreach bfield {1 '-1'} {
+	set xmin $XMIN
+	while {$xmin < $XMAX} {
+		set ymin $YMIN
+		while {$ymin < $YMAX} {
+			puts "Running at $xmin $ymin leaflet $bfield"
+			set data [get_count_with_area $area $xmin $ymin "resname DPPC and beta $bfield"]
+			puts $outfile $data
+
+			set ymin [expr $ymin + $step]
+		}
+		set xmin [expr $xmin + $step]
 	}
-
-	set xmin [expr $xmin + $step]
 }
 
 close $outfile
