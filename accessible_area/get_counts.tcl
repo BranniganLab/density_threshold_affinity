89b66f4d13f3e4a350a82396a64dbae2a07bec43
EzryStIago <ezrysm@gmail.com>
2023-11-27 19:19:52 -0500

    add stride over frames

--- accessible_area/get_counts.tcl
+++ accessible_area/get_counts.tcl
@@ -1,4 +1,4 @@
-proc get_counts { xmin ymin xmax ymax seltext {molid top} {start 0} {end -1} } {
+proc get_counts { xmin ymin xmax ymax seltext {molid top} {start 0} {end -1} {stride 1}} {
 	set sel [atomselect $molid "$seltext and x > $xmin and x < $xmax and y > $ymin and y < $ymax"]
 	if {$end > 0} {
 		set stop $end
@@ -6,7 +6,7 @@ proc get_counts { xmin ymin xmax ymax seltext {molid top} {start 0} {end -1} } {
 		set stop [molinfo top get numframes]
 	}
 	set data ""
-	for {set frm $start} {$frm < $stop} {incr frm} {
+	for {set frm $start} {$frm < $stop} {incr frm $stride} {
 		$sel frame $frm 
 		$sel update
 		set toadd [$sel num]
@@ -16,11 +16,11 @@ proc get_counts { xmin ymin xmax ymax seltext {molid top} {start 0} {end -1} } {
 	return $data
 }
 
-proc get_count_with_area { area ymin xmin seltext {molid top} {start 0} {end -1}} {
+proc get_count_with_area { area ymin xmin seltext {molid top} {start 0} {end -1} {stride 1} } {
 	set width [expr "$area**(0.5)"]
 	set xmax [expr $xmin+$width]
 	set ymax [expr $ymin+$width]
 
-	set data [get_counts $xmin $ymin $xmax $ymax $seltext $molid $start $end]
+	set data [get_counts $xmin $ymin $xmax $ymax $seltext $molid $start $end $stride]
 	return $data
 }
