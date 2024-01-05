b5827fe68b9a66523afe8f324df0a6e50b65ced1
Jesse Sandberg <js2746@belenus.ccib.rutgers.edu>
2024-01-05 11:14:58 -0500

    cleaned up GPCR assign helices script

---
+++ TCL/assign_helices_GPCR_general_JS.tcl
@@ -0,0 +1,34 @@
+proc assign_helices {pdb_type} {
+	set helix_code_list [list 1 2 3 4 5 6 7]
+
+	if {$pdb_type == "atomistic"} {
+		#canonical numbering 
+		set start_list [list 32 67 101 146 196 267 305]
+		set end_list [list 62 96 136 171 231 300 329]
+		set res_sel_str "alpha"
+	} elseif {$pdb_type == "Martini"} {
+		#gromacs/Martini numbering
+		set start_list [list 1 36 70 115 165 205 245]
+		set end_list [list 31 65 105 140 200 235 265]
+		set res_sel_str "name BB SC1 to SC4"
+	} else {
+		puts "ERROR: Incorrect file type entered"
+		return
+	}
+
+	set sel [atomselect top "all"]
+	$sel set occupancy 0
+
+	foreach start_res $start_list end_res $end_list helix_code $helix_code_list {
+		puts "$start_res $end_res $helix_code"
+		[atomselect top "resid $start_res to $end_res and $res_sel_str"] set occupancy $helix_code
+	}
+
+	[atomselect top "$res_sel_str"] set chain A 
+	$sel delete
+}
+
+#USER MUST COMMENT OUT VERSION THEY DONT WANT
+
+assign_helices Martini
+#assign_helices atomistic
