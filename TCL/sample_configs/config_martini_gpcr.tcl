289d7510e667a31af29d8532f8297f72ac4ef26e
Jesse Sandberg <js2746@belenus.ccib.rutgers.edu>
2024-01-05 11:33:28 -0500

    fixed minor errors, improved documentation, and changed defaults to 1 for center and qwrap

--- TCL/sample_configs/config_martini_gpcr.tcl
+++ TCL/sample_configs/config_martini_gpcr.tcl
@@ -5,8 +5,8 @@
 set LEAFLET_SORTING_ALGORITHM 2; 
 
 
-set CENTER_AND_ALIGN 0
-set USE_QWRAP 0
+set CENTER_AND_ALIGN 1
+set USE_QWRAP 1
 set UTILS "./helpers" 
 
 set dt 1
@@ -18,18 +18,18 @@ set backbone_selstr "name BB" ;#selection string used to define the protein back
 set protein_selstr "name BB SC1 to SC4" ;#selection string used to define the entire protein
 
 set lipids [list "CHOL"] ;# list of all species to bin
-set headnames [list "ROH"] ; #lists one headgroup atom/bead name per lipid species 
-set tailnames [list "C2"] ; #lists one terminal atom/bead name per lipid species 
-set lipidbeads_selstrs [list "all"]; #lists one selection string per lipid species; indicates which lipid atom/beads should be counted in the density plot 
-set acylchain_selstrs $lipidbeads_selstrs; #list of beads used to determine chain length; same format as lipidbeads_selstrs. Was originally intented to only hold selection strings containing the acyl chain beads.  
+set headnames [list "ROH"] ;#lists one headgroup atom/bead name per lipid species 
+set tailnames [list "C2"] ;#lists one terminal atom/bead name per lipid species 
+set lipidbeads_selstrs [list "all"] ;#lists one selection string per lipid species; indicates which lipid atom/beads should be counted in the density plot 
+set acylchain_selstrs $lipidbeads_selstrs ;#list of beads used to determine chain length; same format as lipidbeads_selstrs. Was originally intended to only hold selection strings containing the acyl chain beads.  
 
 set chainlist [list A] ;#list of chain names for the protein
-set helixlist [list 1 2 3 4 5 6 7]; #indices for individual secondary structure elements 
-set helix_assignment_script assign_helices_ELIC_general.tcl ;# script that will assigns occupancies in helixlist to different secondary structure elements 
+set helixlist [list 1 2 3 4 5 6 7] ;#indices for individual secondary structure elements 
+set helix_assignment_script assign_helices_GPCR_general.tcl ;# script that will assigns occupancies in helixlist to different secondary structure elements 
 set midplane_selstr "occupancy 1 to 7" ;# selection that includes all transmembrane helices
 
-set Rmax 20. ;##maximum radius of polar density map
-set Rmin 0. ;#minimum radius
-set dr 1 ;#radial bin width 
-set Ntheta 50; #number of angular bins 
+set Rmax 20. ;##maximum radius of polar density map (in Angstroms)
+set Rmin 0. ;#minimum radius (in Angstroms)
+set dr 1 ;#radial bin width (in Angstroms)
+set Ntheta 50 ;#number of angular bins 
 
