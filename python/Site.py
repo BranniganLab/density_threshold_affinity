486183b50116d83aa19c1edd84a190fd2157bdb5
Jesse Sandberg <js2746@belenus.ccib.rutgers.edu>
2024-10-17 13:02:09 -0400

    added fetch site counts and parser

--- python/Site.py
+++ python/Site.py
@@ -11,6 +11,9 @@ import warnings
 import math
 from pathlib import Path
 from scipy import constants
+from collections import namedtuple
+
+dimensions = namedtuple('dimensions', ['dr', 'Nr', 'dtheta', 'Ntheta', 'Nframes'])
 
 
 class Site:
@@ -183,7 +186,7 @@ class Site:
             equal accessible area to the site.
 
         """
-        return self.calculate_hist_mode(bulk=True, nonzero=False)
+        return self._calculate_hist_mode(bulk=True, nonzero=False)
 
     @property
     def dG(self):
@@ -197,9 +200,9 @@ class Site:
             The total binding affinity, in kcal/mol.
 
         """
-        return self.calculate_dG(bulk=False) - self.calculate_dG(bulk=True)
+        return self._calculate_dG(bulk=False) - self._calculate_dG(bulk=True)
 
-    def calculate_dG(self, bulk=False):
+    def _calculate_dG(self, bulk=False):
         """
         Calculate the delta G. If bulk is True, this value is dG_ref. If bulk \
         is False, this value is dG_site.
@@ -216,11 +219,11 @@ class Site:
 
         """
         minus_RT = -1.0 * self.temp * constants.R / 4184.  # kcal/mol
-        P_unnoc = self.calculate_P_unnoc(bulk=bulk)
+        P_unnoc = self._calculate_P_unnoc(bulk=bulk)
         delta_G = minus_RT * np.log((1 - P_unnoc) / P_unnoc)
         return delta_G
 
-    def calculate_hist_mode(self, bulk=False, nonzero=False):
+    def _calculate_hist_mode(self, bulk=False, nonzero=False):
         """
         Calculate the mode of the counts histogram.
 
@@ -228,7 +231,7 @@ class Site:
         ----------
         nonzero : boolean
             If True, in the case of mode=0, use the second highest peak instead.\
-            This is necessary when estimating the predicted accessible area.
+            This may be necessary when estimating the predicted accessible area.
 
         Returns
         -------
@@ -250,7 +253,7 @@ class Site:
                 mode = np.argmax(hist[1:]) + 1
         return mode
 
-    def calculate_hist_mean(self, bulk=False):
+    def _calculate_hist_mean(self, bulk=False):
         """
         xtz.
 
@@ -276,17 +279,19 @@ class Site:
         mean = sum_i / total_N
         return mean
 
-    def update_counts_histogram(self, filepath, bulk=False):
+    def update_counts_histogram(self, bulk, data):
         """
         Assign ligand bead counts to Site attribute "counts_histogram".
 
         Parameters
         ----------
-        filepath : Path
-            The path to the .dat file to be parsed.
         bulk : boolean
             If True, update the counts histogram for the bulk patch. If False,\
-            update the counts histogram for the site. Default is False.
+            update the counts histogram for the site.
+        data : ndarray or str or Path
+            If bulk=True, provide the path (str or Path) to the .dat file from \
+            do_get_counts.tcl. If bulk=False, provide the 3D ndarray containing\
+            binned counts information.
 
         Returns
         -------
@@ -294,15 +299,51 @@ class Site:
 
         """
         if bulk:
-            counts = np.loadtxt(filepath).astype(int).flatten()
-            hist = np.bincount(counts)
-            self._bulk_counts_histogram = hist
+            if isinstance(data, str):
+                filepath = Path(data)
+            elif isinstance(data, Path):
+                filepath = data
+            else:
+                raise Exception("Must provide the path to the bulk counts.")
+            assert filepath.exists(), f"Could not find {filepath}"
+            assert filepath.is_file(), f"This is not recognized as a file {filepath}"
+            assert filepath.suffixes[1] == '.dat', "You must provide the .dat file output from do_get_counts.tcl in VMD."
+            bulk_counts = np.loadtxt(filepath).astype(int).flatten()
+            bulk_hist = np.bincount(bulk_counts)
+            self._bulk_counts_histogram = bulk_hist
         else:
-            counts = self.fetch_site_counts(filepath)
-            hist = np.bincount(counts)
-            self._site_counts_histogram = hist
+            assert isinstance(data, np.ndarray), "ndarray not supplied"
+            assert len(data.shape) == 3, "This ndarray is not 3 dimensional"
+            site_counts = self._fetch_site_counts(data)
+            site_hist = np.bincount(site_counts)
+            self._site_counts_histogram = site_hist
 
-    def calculate_P_unnoc(self, bulk=False):
+    def _fetch_site_counts(self, binned_counts):
+        """
+        Create a 2D array where each row is a different bin within the site \
+        and each column is a frame in the trajectory. Then sum over all the \
+        rows to get site counts rather than bin counts.
+
+        Parameters
+        ----------
+        binned_counts : ndarray
+            3D ndarray containing the binned counts from polarDensityBin.tcl
+
+        Returns
+        -------
+        site_counts : ndarray
+            1D ndarray containing the site counts for each frame of the \
+            trajectory.
+
+        """
+        stack = np.zeros(binned_counts.shape[0])
+        for bin_tuple in self.bin_coords:
+            r_bin, theta_bin = bin_tuple
+            stack = np.vstack((stack, binned_counts[:, r_bin, theta_bin]))
+        site_counts = np.sum(stack, axis=0)
+        return site_counts
+
+    def _calculate_P_unnoc(self, bulk=False):
         """
         Calculate the probability that the site is unoccupied by ligand. This \
         is defined by the portion of the histogram <= n_peak divided by the \
@@ -329,24 +370,6 @@ class Site:
         assert math.isclose(P_occ + P_unnoc, 1, abs_tol=0.01), f"Probabilities do not sum to one. Current sum: {P_unnoc + P_occ}"
         return P_unnoc
 
-
-    def fetch_site_counts(self, filepath):
-        """
-        
-
-        Parameters
-        ----------
-        filepath : TYPE
-            DESCRIPTION.
-
-        Returns
-        -------
-        counts : TYPE
-            DESCRIPTION.
-
-        """
-        return counts
-
     def calculate_geometric_area(self, dr, dtheta):
         """
         Calculate the geometric area of the site.
@@ -370,22 +393,104 @@ class Site:
             area += dr * dtheta * bin_radial_midpoint
         return area
 
-    def predict_accessible_area(self, bulk_area):
+    def predict_accessible_area(self, bulk_area, mode=False):
         """
-        
+        Predict the accessible area of the site. A reasonable method is to \
+        multiply the area of the bulk patch you just analyzed by the ratio of\
+        the means (or modes) for the site distribution and the bulk \
+        distribution. This will put you in the ballpark of the bulk patch area.
 
         Parameters
         ----------
-        bulk_area : TYPE
-            DESCRIPTION.
+        bulk_area : float
+            The area of the bulk patch previously analyzed.
+        mode : boolean
+            If True, use the site and bulk modes rather than the means. Default\
+            is False (use means).
 
         Returns
         -------
-        predicted_accessible_area : TYPE
-            DESCRIPTION.
+        predicted_accessible_area : float
+            The area of the bulk patch you should analyze next to try and more\
+            closely match the site distribution.
 
         """
-        site_mean = self.calculate_hist_mean(bulk=False)
-        bulk_mean = self.calculate_hist_mean(bulk=True)
-        predicted_accessible_area = bulk_area * (site_mean / bulk_mean)
+        if self.ligand != "DPPC":
+            warnings.append("Warning: This feature was originally built for use in a 100% DPPC test system. Proceed with caution.")
+        if mode:
+            site = self._calculate_hist_mode(bulk=False)
+            bulk = self._calculate_hist_mode(bulk=True)
+        else:
+            site = self._calculate_hist_mean(bulk=False)
+            bulk = self._calculate_hist_mean(bulk=True)
+        predicted_accessible_area = bulk_area * (site / bulk)
         return predicted_accessible_area
+
+
+def calculate_nframes(r_values):
+    """
+    Calculate how many frames are in a trajectory from the first column of the \
+    .dat file produced by the TCL script.
+
+    Parameters
+    ----------
+    r_values : ndarray
+        The beginning r values for each bin in your system.
+
+    Returns
+    -------
+    nframes : int
+        The number of frames in your trajectory.
+
+    """
+    match_value = r_values[0]
+    for i in range(len(r_values)):
+        if r_values[i] != match_value:
+            return i
+    return None
+
+
+def parse_tcl_dat_trajectory(filepath, bulk):
+    """
+    Extract bin counts and bin dimensions from a TCL output .dat file.
+
+    Parameters
+    ----------
+    filepath : Path
+        The path to the TCL .dat output file.
+    bulk : boolean
+        If True, assume output is from do_get_counts. If False, assume \
+        output is from polarDensityBin.
+
+    Returns
+    -------
+    counts : ndarray
+        If bulk is True, return a 1D ndarray. If bulk is False, return a 3D \
+        ndarray of integer counts. Dimension 0 is time, dimension 1 is r and \
+        dimension 2 is theta. IE the count for the 4th radial bin and 12th \
+        theta bin on the 33rd frame of the trajectory would be counts[32, 3, 11].
+    dimensions : namedtuple or None
+        If bulk is True, return None. If bulk is False, return the bin \
+        dimensions in r and theta as well as the number of frames inside a \
+        namedtuple.
+
+    """
+    if bulk:
+        return np.loadtxt(filepath).astype(int).flatten(), None
+    else:
+        unrolled_data = np.loadtxt(filepath, skiprows=1, delimiter=' ')
+        dr = unrolled_data[0, 1] - unrolled_data[0, 0]
+        dtheta = unrolled_data[0, 2]
+        nframes = calculate_nframes(unrolled_data[:, 0])
+        Ntheta = int(round(360 / dtheta))
+        assert Ntheta == unrolled_data.shape[1] - 3, f"Something went wrong with the theta dimensions parser. dtheta={dtheta}, Ntheta={Ntheta}"
+        Nr = len(unrolled_data[:, 0]) / nframes
+        assert Nr - int(Nr) == 0, f"Something went wrong with the r dimensions parser. dr={dr}, Nr={Nr}"
+        Nr = int(Nr)
+        grid_dims = dimensions(dr, Nr, dtheta, Ntheta, nframes)
+        unrolled_counts = unrolled_data[:, 3:].astype(int)
+        sideways_counts = np.zeros((Nr, nframes, Ntheta))
+        for i in range(Nr):
+            sideways_counts[i, :, :] = unrolled_counts[(nframes * i):(nframes * (i + 1)), :]
+        counts = np.swapaxes(sideways_counts, 0, 1)
+        return counts, grid_dims
