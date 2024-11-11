613c5c0316a7a4b03ba25398c29088d5b7f0978b
Jesse Sandberg <js2746@belenus.ccib.rutgers.edu>
2024-11-11 14:54:41 -0500

    added cmap replacement

---
+++ python/plotting.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 11 14:41:24 2024.
+
+@author: js2746
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.colors import ListedColormap
+
+
+def make_custom_colormap():
+    """
+    Make a custom colormap for plotting.
+
+    Returns
+    -------
+    my_cmap : matplotlib colormap
+        The colormap to use when plotting.
+
+    """
+    depleted = plt.colormaps['RdGy_r']
+    enriched = plt.colormaps['bwr']
+    newcolors = np.concatenate([depleted(np.linspace(0.35, 0.5, 128)), enriched(np.linspace(0.5, 1, 128))])
+    my_cmap = ListedColormap(newcolors)
+    return my_cmap
