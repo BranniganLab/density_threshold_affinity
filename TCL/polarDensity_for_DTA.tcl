b5aad1184441604e385774521fba2dde595b8be9
Jesse Sandberg <js2746@belenus.ccib.rutgers.edu>
2025-03-14 12:43:38 -0400

    minor fix

--- TCL/polarDensity_for_DTA.tcl
+++ TCL/polarDensity_for_DTA.tcl
@@ -372,7 +372,7 @@ proc frame_leaflet_assignment {species headname tailname lipidbeads_selstr frame
 proc trajectory_leaflet_assignment {species headname tailname lipidbeads_selstr} { 
     global params
     set num_reassignments 0
-    if {[lsearch -integer -exact "0 1 2 3" $params(leaflet_sorting_algorithm)] == -1} {
+    if {[lsearch -exact "0 1 2 3" $params(leaflet_sorting_algorithm)] == -1} {
         puts "Option $params(leaflet_sorting_algorithm) not recognized as a leaflet sorting option. Defaulting to option 1."
     } elseif {$params(leaflet_sorting_algorithm) == 2} {
         if {$params(leaflet_sorter_2_reference_sel) eq "none"} {
