b91fa4b3ba8f1c6e88a4cb91a6c545d0ae76e866
Jesse Sandberg <js2746@belenus.ccib.rutgers.edu>
2025-03-17 09:00:28 -0400

    switched beads and lipids to atoms

--- TCL/polarDensity_for_DTA.tcl
+++ TCL/polarDensity_for_DTA.tcl
@@ -439,15 +439,15 @@ proc theta_histogram {singleFrame_lower singleFrame_upper } {
 }
 
 
-;#The inner-most loop of the histogramming algorithm: a loop over all lipids occupying one shell in one frame. Each lipid is assigned an angular bin and totals are updated.  
-proc loop_over_lipids {shell species frm} {
+;#The inner-most loop of the histogramming algorithm: a loop over all lipid atoms (or beads) occupying one shell in one frame. Each atom is assigned an angular bin and totals are updated.  
+proc loop_over_atoms {shell species frm} {
     global params
     set indexs [$shell get index]
     set theta_high_out [list]
     set theta_low_out [list]
     set leaflet 0
     foreach indx $indexs {
-        #loop over lipids in the shell
+        #loop over atoms in the shell
         set atsel "($species and index $indx)"
         set thislipid [atomselect top $atsel frame $frm]
         set x [$thislipid get x]
@@ -460,7 +460,7 @@ proc loop_over_lipids {shell species frm} {
         } elseif {$leaflet < 0} {
             lappend theta_low_out $theta_bin
         } else {
-            puts "WARNING: lipid bead $indx did not get assigned a leaflet for frame $frm"
+            puts "WARNING: lipid atom $indx did not get assigned a leaflet for frame $frm"
         }
         $thislipid set user [expr $theta_bin + 1]
         $thislipid delete
@@ -477,7 +477,7 @@ proc loop_over_frames {shell species headname tailname lipidbeads_selstr start_f
     for {set frm $params(start_frame)} {$frm < ${end_frame}} {incr frm $params(dt)} {
         $shell frame $frm
         $shell update 
-        set singleFrame_counts [loop_over_lipids $shell $species $frm]
+        set singleFrame_counts [loop_over_atoms $shell $species $frm]
         set singleFrame_upper [lindex $singleFrame_counts 1] 
         set singleFrame_lower [lindex $singleFrame_counts 0]
         set theta_bins [theta_histogram $singleFrame_lower $singleFrame_upper]
