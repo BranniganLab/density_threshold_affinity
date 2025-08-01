62df4c6a1b84d065bdf693350186281629d36080
Jesse Sandberg <js2746@belenus.ccib.rutgers.edu>
2025-08-01 10:07:17 -0400

    added leaflet_sorter_4 which just assigns everything in the system to user2=1

--- TCL/polarDensity_for_DTA.tcl
+++ TCL/polarDensity_for_DTA.tcl
@@ -312,6 +312,10 @@ proc leaflet_detector {atsel_in head tail frame_i leaflet_sorting_algorithm} {
         leaflet_sorter_2 $atsel_in $params(leaflet_sorter_2_reference_sel) $frame_i
     } elseif { $leaflet_sorting_algorithm == 3 } {
         leaflet_sorter_3 $atsel_in $frame_i
+    } elseif { $leaflet_sorting_algorithm == 4} {
+        set sel [atomselect top "all" frame $frame_i]
+        $sel set user2 1
+        $sel delete
     } else { 
         #default
         leaflet_sorter_1 $atsel_in $frame_i
@@ -341,7 +345,7 @@ proc frame_leaflet_assignment {atseltext headname tailname frame_i frame_f {rest
         #assign leaflets from $frame_i to user2 field of each bead for this selection
         foreach sel_resid $sel_resid_list {
             set selstring "(${atseltext}) and (resid $sel_resid)"
-            set leaflet [leaflet_detector $selstring $headname $tailname $frame_i $params(leaflet_sorting_algorithm)]
+            leaflet_detector $selstring $headname $tailname $frame_i $params(leaflet_sorting_algorithm)
         }
         #copy leaflet values from $frame_i to all frames between $frame_i and $frame_f
         set leaflet_list [$sel get user2] 
@@ -368,7 +372,7 @@ proc frame_leaflet_assignment {atseltext headname tailname frame_i frame_f {rest
 proc trajectory_leaflet_assignment {atseltext headname tailname} { 
     global params
     set num_reassignments 0
-    if {[lsearch -exact "0 1 2 3" $params(leaflet_sorting_algorithm)] == -1} {
+    if {[lsearch -exact "0 1 2 3 4" $params(leaflet_sorting_algorithm)] == -1} {
         puts "Option $params(leaflet_sorting_algorithm) not recognized as a leaflet sorting option. Defaulting to option 1."
     } elseif {$params(leaflet_sorting_algorithm) == 2} {
         if {$params(leaflet_sorter_2_reference_sel) eq "none"} {
@@ -634,7 +638,8 @@ proc polarDensityBin { config_file_script } {
         set upp_f [open "${stem}.upp.dat" w]
         set low_f_avg [open "${stem}.low.avg.dat" w]
         set upp_f_avg [open "${stem}.upp.avg.dat" w]
-        set totals [frame_leaflet_assignment $atseltext $headname $tailname $params(end_frame) $params(end_frame)]        
+        set totals [frame_leaflet_assignment $atseltext $headname $tailname $params(end_frame) $params(end_frame)]   
+        puts $totals     
         
         foreach lu [list $low_f $upp_f] avgfile [list $low_f_avg $upp_f_avg] leaf_total $totals {
             set leaflet_str [lindex $leaf_total 0]
