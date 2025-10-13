4c54b5404b044b708dfc6ad52318c6c09266e8e9
Jesse Sandberg <js2746@belenus.ccib.rutgers.edu>
2025-10-13 16:27:02 -0400

    moved functions to density to prevent circular import

--- python/DTA/utils.py
+++ python/DTA/utils.py
@@ -9,7 +9,6 @@ import numpy as np
 from scipy import constants
 import math
 from pathlib import Path
-from DTA.density import parse_tcl_dat_file, Dimensions
 
 
 def calculate_dG(counts_histogram, n_peak, temperature):
@@ -196,49 +195,6 @@ def check_bulk_counts_histogram(site_list):
     return bulk
 
 
-def load_replica_counts(root_path, replicas_list, system_name, leaflet_id, avg=False):
-    """
-    Load the counts from all replicas of a single system. Return them as a list.
-
-    Parameters
-    ----------
-    root_path : str or Path
-        The path to directory containing your replica subdirectories.
-    replicas_list : list
-        The list of replica subdirectory names.
-    system_name : str
-        The file stem for your PolarDensityBin outputs.
-    leaflet_id : int
-        Which leaflet your Site is in. 1=outer and 2=inner.
-    avg : bool
-        If True, load the average density file. If False (default), load the \
-        individual frame counts.
-
-    Returns
-    -------
-    list of ndarrays.
-
-    """
-    assert leaflet_id in [1, 2], "leaflet_id must be 1 (outer) or 2 (inner)."
-    assert isinstance(root_path, (str, Path)), "root_path must be a str or Path."
-    if isinstance(root_path, str):
-        root_path = Path(root_path)
-    assert root_path.exists(), f"could not find root_path {root_path}"
-    assert isinstance(replicas_list, list), "replicas_list must be a list."
-    assert (len(replicas_list) > 1), "Less than 2 replicas found."
-    replica_counts_list = []
-    leaflet = {1: "upp", 2: "low"}
-    for rep in replicas_list:
-        if avg:
-            fname = root_path.joinpath(rep, f"{system_name}.{leaflet[leaflet_id]}.avg.dat")    
-        else:
-            fname = root_path.joinpath(rep, f"{system_name}.{leaflet[leaflet_id]}.dat")
-        assert fname.is_file(), f"could not find file {fname}"
-        counts, grid_dims, system_info = parse_tcl_dat_file(fname, bulk=False)
-        replica_counts_list.append(counts)
-    return replica_counts_list
-
-
 def validate_path(path, file=False):
     """
     Make sure that a path can be found. If file=True, make sure that it is \
@@ -273,33 +229,3 @@ def validate_path(path, file=False):
         if not path.is_file():
             raise FileNotFoundError(f"The specified file '{path}' is not recognized as a file. Please ensure the path is correct.")
     return path
-
-
-def valid_Dimensions(list_of_Dimensions_objs):
-    """
-    Make sure that all Dimensions attributes do not vary across objects \
-        (not counting Nframes) in a list of Dimensions objects. If attributes \
-        do not vary across objects, return True. Else, return False.
-
-    Parameters
-    ----------
-    list_of_Dimensions_objs : list
-        List containing multiple Dimensions objects.
-
-    Returns
-    -------
-    Boolean
-
-    """
-    if not isinstance(list_of_Dimensions_objs, list):
-        raise TypeError(f"grid_dims must be a list, not a {type(list_of_Dimensions_objs)}.")
-    if not all(isinstance(item, Dimensions) for item in list_of_Dimensions_objs):
-        raise TypeError(f"grid_dims_list must contain Dimensions namedtuples.")
-    if len(list_of_Dimensions_objs) == 1:
-        return True
-    dr, Nr, dtheta, Ntheta, _ = list_of_Dimensions_objs[0]
-    for item in list_of_Dimensions_objs:
-        for attr, val in zip(["dr", "Nr", "dtheta", "Ntheta"], [dr, Nr, dtheta, Ntheta]):
-            if item[attr] != val:
-                return False
-    return True
