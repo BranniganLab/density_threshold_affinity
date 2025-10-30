81d464fb8e8527e5dca6b80415049d25901ba10b
Jesse Sandberg <js2746@belenus.ccib.rutgers.edu>
2025-10-30 12:30:18 -0400

    removed all old files and amended init

--- python/DTA/__init__.py
+++ python/DTA/__init__.py
@@ -7,14 +7,7 @@ Tools for Doing Density-Threshold Affinity analysis
 __version__ = "1.0"
 __author__ = 'Brannigan Lab'
 __credits__ = 'Rutgers University - Camden'
-__all__=['Density_Analysis',
-         'enrichment_plotters',
-        'ParsingGathering',
-        'Polar_Binning_DeltaG',
-        'polarDensity_helper',
-        'polarRadial_Complex_helper',
-        'site_distributions',
-        'Site',
+__all__=['Site',
         'SymmetricSite',
         'SiteAcrossReplicas',
         'utils',
@@ -22,13 +15,6 @@ __all__=['Density_Analysis',
         'density']
 
 
-from .Density_Analysis import *
-from .enrichment_plotters import *
-from .ParsingGathering import *
-from .Polar_Binning_DeltaG import *
-from .polarDensity_helper import *
-from .polarRadial_Complex_helper import *
-from .site_distributions import *
 from .Site import *
 from .SiteAcrossReplicas import *
 from .SymmetricSite import *
