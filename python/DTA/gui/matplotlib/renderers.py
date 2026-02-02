443b1449a6355ffc29a037b6b1589b51a19cdad5
Jesse Sandberg <js2746@viscosus.ccib.rutgers.edu>
2026-02-02 16:27:16 -0500

    finished renderers

---
+++ python/DTA/gui/matplotlib/renderers.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Matplotlib renderers for polar bin selection.
+
+This module contains view-layer classes responsible for drawing
+polar bin grids, selection outlines, and transient preview geometry
+using Matplotlib.
+
+Renderers are strictly passive:
+- they read geometry and selection state from dta.core objects,
+- they draw current and preview visuals onto Matplotlib Axes,
+- they do not interpret user input or mutate selection state.
+
+All event handling and gesture interpretation is handled by
+Matplotlib controllers in selectors.py. Persistent selection
+state lives in dta.core.selection.
+"""
+
+
+class PolarBinRenderer:
+    """
+    Renderer for drawing polar objects on a matplotlib Axes.
+
+    This class converts internal (r, theta) geometry into the
+    coordinate order expected by matplotlib polar plots.
+    """
+
+    def __init__(self, ax, plot_kwargs=None):
+        """
+        Create a PolarBinRenderer object and tie it to an Axes instance.
+
+        Parameters
+        ----------
+        ax : matplotlib.axes.Axes
+            Polar axes used for drawing.
+        plot_kwargs : dict
+            Dictionary of matplotlib.pyplot keywords for drawing bin edges.
+        """
+        self.ax = ax
+        default_plot_kwargs = {
+            "color": "red",
+            "lw": 2.0,
+            "zorder": 20
+        }
+        if plot_kwargs is None:
+            self.plot_kwargs = default_plot_kwargs
+        else:
+            self.plot_kwargs = default_plot_kwargs | plot_kwargs
+
+    def draw_edges(self, edges, plot_kwargs=None):
+        """
+        Draw a collection of bin edges.
+
+        Parameters
+        ----------
+        edges : iterable of BinEdge
+            Edges to draw.
+        plot_kwargs : dict
+            Keyword arguments passed to ``Axes.plot``.
+
+        Returns
+        -------
+        list
+            Matplotlib artist objects created.
+        """
+        artists = []
+        kwargs = dict(self.plot_kwargs)
+        if plot_kwargs is not None:
+            kwargs.update(plot_kwargs)
+
+        for edge in edges:
+            artists.append(self.ax.plot(edge.theta_endpoints, edge.r_endpoints, **kwargs)[0])
+        return artists
+
+    def shade_interior_region(self):
+        """Hold space for future method."""
