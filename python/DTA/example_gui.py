9480fe5057a4d80bc41374c6f0f1c1135a1ca39c
Jesse Sandberg <js2746@viscosus.ccib.rutgers.edu>
2026-02-02 17:11:34 -0500

    fixed path

--- python/DTA/example_gui.py
+++ python/DTA/example_gui.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import matplotlib.pyplot as plt
-from dta.gui.matplotlib import SiteSelectorManager, SiteSelector
+from DTA.gui.matplotlib import SiteSelectorManager, SiteSelector
 
 
 def example_usage():
