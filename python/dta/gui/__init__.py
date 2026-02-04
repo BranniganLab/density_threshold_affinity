68ff4773995ab39298229b039183106be391a376
Jesse Sandberg <sandberg.jesse@gmail.com>
2026-02-03 20:43:22 -0500

    moved up a level

--- python/dta/gui/__init__.py
+++ python/dta/gui/__init__.py
@@ -1,12 +1,1 @@
-__all__ = ["SiteSelector", "SiteSelectorManager", "PolarBinRenderer"]
 
-def __getattr__(name: str):
-    if name in __all__:
-        from .matplotlib import selectors, renderers
-        mapping = {
-            "SiteSelector": selectors.SiteSelector,
-            "SiteSelectorManager": selectors.SiteSelectorManager,
-            "PolarBinRenderer": renderers.PolarBinRenderer,
-        }
-        return mapping[name]
-    raise AttributeError(name)
