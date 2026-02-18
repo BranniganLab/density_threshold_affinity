266b8652ae85593aefab01a22969c13f472b8250
Jesse Sandberg <js2746@viscosus.ccib.rutgers.edu>
2026-02-18 14:54:14 -0500

    added docstring

--- python/dta/example_gui.py
+++ python/dta/example_gui.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""Example usage of the SiteSelector GUI."""
 import numpy as np
 import matplotlib.pyplot as plt
 from dta.gui import SiteSelectorManager, SiteSelector
