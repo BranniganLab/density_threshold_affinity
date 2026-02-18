021b1e687677c316af53191e99fe3dcb9a1cc31d
Jesse Sandberg <js2746@viscosus.ccib.rutgers.edu>
2026-02-18 14:49:47 -0500

    added docstring

--- python/dta/gui/__init__.py
+++ python/dta/gui/__init__.py
@@ -1,3 +1,4 @@
+"""Import the necessary classes."""
 from .selectors import SiteSelector, SiteSelectorManager
 from .state import SelectionOperation, SelectorDragState
 from .renderers import SelectionRenderer
