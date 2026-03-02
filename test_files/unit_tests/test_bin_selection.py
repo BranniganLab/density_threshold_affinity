d96075a3577fbb0b7ce113869d98a55541b0df93
Jesse Sandberg <js2746@viscosus.ccib.rutgers.edu>
2026-03-02 15:14:40 -0500

    added BinAddress to BinSelection tests

--- test_files/unit_tests/test_bin_selection.py
+++ test_files/unit_tests/test_bin_selection.py
@@ -6,7 +6,7 @@ Created on Mon Mar  2 14:13:46 2026
 @author: js2746
 """
 
-from dta.bin_logic import BinSelection
+from dta.bin_logic import BinSelection, BinAddress
 
 
 def test_bin_selection_initially_empty():
@@ -18,10 +18,10 @@ def test_bin_selection_update_replaces_selection():
     sel = BinSelection()
 
     sel.set_bins([(0, 1), (2, 3), (0, 1)])  # duplicates should be collapsed
-    assert sel.get_bins() == {(0, 1), (2, 3)}
+    assert sel.get_bins() == {BinAddress(0, 1), BinAddress(2, 3)}
 
     sel.set_bins([(9, 9)])
-    assert sel.get_bins() == {(9, 9)}
+    assert sel.get_bins() == {BinAddress(9, 9)}
 
 
 def test_bin_selection_clear_empties_selection():
@@ -36,11 +36,11 @@ def test_bin_selection_get_bins_returns_copy_not_alias():
     sel.set_bins([(1, 1), (2, 2)])
 
     bins_copy = sel.get_bins()
-    bins_copy.add((999, 999))  # mutate the returned set
+    bins_copy.add(BinAddress(999, 999))  # mutate the returned set
 
     # internal state should be unchanged
-    assert sel.get_bins() == {(1, 1), (2, 2)}
-    assert (999, 999) not in sel.get_bins()
+    assert sel.get_bins() == {BinAddress(1, 1), BinAddress(2, 2)}
+    assert BinAddress(999, 999) not in sel.get_bins()
 
 
 def test_bin_selection_update_accepts_any_iterable():
@@ -48,8 +48,14 @@ def test_bin_selection_update_accepts_any_iterable():
 
     # generator input
     sel.set_bins((i, i + 1) for i in range(3))
-    assert sel.get_bins() == {(0, 1), (1, 2), (2, 3)}
+    assert sel.get_bins() == {BinAddress(0, 1), BinAddress(1, 2), BinAddress(2, 3)}
 
     # set input
     sel.set_bins({(5, 6), (7, 8)})
-    assert sel.get_bins() == {(5, 6), (7, 8)}
+    assert sel.get_bins() == {BinAddress(5, 6), BinAddress(7, 8)}
+
+
+def test_bin_selection_accepts_binaddress_inputs_too():
+    sel = BinSelection()
+    sel.set_bins([BinAddress(3, 4), BinAddress(3, 4), BinAddress(8, 9)])
+    assert sel.get_bins() == {BinAddress(3, 4), BinAddress(8, 9)}
