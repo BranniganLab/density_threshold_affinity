b088a1bf24e3ef3b9e969425ece8d0de0727798e
Jesse Sandberg <js2746@viscosus.ccib.rutgers.edu>
2026-03-02 14:22:38 -0500

    fixed init

--- python/dta/gui/__init__.py
+++ python/dta/gui/__init__.py
@@ -1,4 +1,4 @@
 """Import the necessary classes."""
 from .selectors import SiteSelector, SiteSelectorManager
-from .selector_state import SelectionOperation, SelectorDragState, BinSelection
+from .selector_state import SelectionOperation, SelectorDragState
 from .renderers import SelectionRenderer
