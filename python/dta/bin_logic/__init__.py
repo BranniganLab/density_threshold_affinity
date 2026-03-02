372a8a505003762dbd0c1adc67ff525eaa606377
Jesse Sandberg <js2746@viscosus.ccib.rutgers.edu>
2026-03-02 15:47:38 -0500

    fixed init

--- python/dta/bin_logic/__init__.py
+++ python/dta/bin_logic/__init__.py
@@ -1,3 +1,3 @@
 """Import the classes and functions."""
-from .utils import BinEdge, BinAddress, Coordinate, unwrap_theta
+from .utils import BinEdge, BinAddress, Coordinate, unwrap_theta, CoordinateLike, as_coordinate
 from .bin_selection import BinSelection
