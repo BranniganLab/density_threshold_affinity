e41b6fbcee63884a93110bc0274100eb9973636e
Jesse Sandberg <js2746@viscosus.ccib.rutgers.edu>
2026-05-07 15:15:34 -0400

    fixed polarbingrid

--- test_files/unit_tests/test_site_selector_matplotlib.py
+++ test_files/unit_tests/test_site_selector_matplotlib.py
@@ -35,19 +35,17 @@ def test_renderer_draw_edges_matches_manual(fig_test, fig_ref):
 
 @check_figures_equal(extensions=("png",))
 def test_selector_draw_selection_matches_expected(fig_test, fig_ref):
-    theta_edges = np.linspace(0, 2 * np.pi, 9)
-    r_edges = np.linspace(0, 1, 5)
+    grid = PolarBinGrid(0, 1, 4, 8)
     bins = {(0, 0), (0, 1), (1, 1)}
 
     # test figure: selector draws committed selection
     ax_t = fig_test.add_subplot(111, projection="polar")
-    sel = SiteSelector(ax_t, theta_edges, r_edges, plot_kwargs={"color": "r", "lw": 2, "zorder": 20})
+    sel = SiteSelector(ax_t, grid, plot_kwargs={"color": "r", "lw": 2, "zorder": 20})
     sel.selection.set_bins(bins)
     sel._draw_selection()
 
     # reference figure: compute edges and draw manually
     ax_r = fig_ref.add_subplot(111, projection="polar")
-    grid = PolarBinGrid(theta_edges, r_edges)
     edges = grid.exposed_edges(bins)
     for e in edges:
         theta_endpoints = e.endpoint1[1], e.endpoint2[1]
