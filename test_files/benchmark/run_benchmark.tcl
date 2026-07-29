479e003754ddd5b22aad8f2435280a0f5efd2ac8
Jesse Sandberg <js2746@viscosus.ccib.rutgers.edu>
2026-07-29 16:42:53 -0400

    created benchmark script

---
+++ test_files/benchmark/run_benchmark.tcl
@@ -0,0 +1,17 @@
+set scriptDir [file dirname [file normalize [info script]]]
+set DTA_path [file normalize [file join $scriptDir "../TCL/polarDensity_for_DTA.tcl"]]
+source $DTA_path
+
+proc load_traj {trajpath groname xtcname} {
+	mol new $trajpath$groname
+	if {$xtcname != 0} {
+		mol addfile $trajpath$xtcname waitfor all
+		animate delete beg 0 end 0 skip 0 top
+	}
+}
+
+load_traj "../MD_files/rep1/" "example.gro" "example.xtc"
+
+proc RunBenchmark {} {
+	polarDensityBin "../MD_files/rep1/config_for_regr_test.tcl"
+}
